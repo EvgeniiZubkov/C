86c150d72a6ea1075cda1cbc42c0b29b96b21c3b
Evgeny <fanofsquirrels@gmail.com>
2022-10-13 11:50:51 +0300

    Task01 Done

---
+++ Task01/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("введите первое целое число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе целое число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if(number1 == number2 * number2)
+{Console.WriteLine("Верно!");
+}
+else
+{Console.WriteLine("Не верно!");
+}
