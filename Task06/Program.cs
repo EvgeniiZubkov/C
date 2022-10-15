ba430b59b7dec6548353e66cc4da130b8ba2f9f3
Evgeny <fanofsquirrels@gmail.com>
2022-10-15 12:56:44 +0300

    Домашка 06

--- Task06/Program.cs
+++ Task06/Program.cs
@@ -2,6 +2,6 @@
 
 Console.WriteLine("Введите четное целое число");
 int number = Convert.ToInt32(Console.ReadLine());
-if (number / 2)
-    Console.WriteLine("Число четное");
-else Console.WriteLine("Число не четное");
+if (number % 2 == 0)
+    Console.WriteLine("Число является четным");
+else Console.WriteLine("Число не является четным");
