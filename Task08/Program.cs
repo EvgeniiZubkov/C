9c157290521c80b162dc0c9c89bfe980367a510f
Evgeny <fanofsquirrels@gmail.com>
2022-10-15 13:51:40 +0300

    Task08 в процессе

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -4,3 +4,13 @@
 // 8 -> 2, 4, 6, 8
 
 Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+int print = 0;
+while (count < number)
+{
+    if (print == count % 2)
+        Console.WriteLine(print);
+    else
+    count = count + 1;
+}
