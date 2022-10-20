dcb99cdb0930c71322200f0766b479ba9722912c
Evgeny <fanofsquirrels@gmail.com>
2022-10-20 12:27:26 +0300

    Выполнено

--- Task22/Program.cs
+++ Task22/Program.cs
@@ -2,8 +2,28 @@
 // принимает на вход число (N) и выдаёт таблицу
 // квадратов чисел от 1 до N.
 
-System.Console.WriteLine("Введите число: ");
+Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+// void SquareTable(int num)
+// {
+//     if (num > 0)
+//     {
+//         for (int i = 1; i <= num; i++)
+//         {
+//             Console.WriteLine($"|{i,4} | {i * i,4}|");
+//         }
+//     }
+//     else
+//     {
+//         Console.WriteLine("Введите значение больше 0");
+//     }
+// }
+
+// SquareTable(n);
 
 void Square(int x)
 {
