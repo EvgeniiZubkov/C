90d8c035038ecb970ed44addf4879067f99693d2
Evgeny <fanofsquirrels@gmail.com>
2022-11-07 12:40:30 +0300

    Выполнено

--- Task28/Program.cs
+++ Task28/Program.cs
@@ -9,7 +9,7 @@ int num = Convert.ToInt32(Console.ReadLine());
 
 int SumNumbers(int number)
 {
-    int sum = 1; // 0
+    int sum = 1;
 
     for (int i = 1; i <= number; i++)
     {
@@ -22,3 +22,28 @@ int SumNumbers(int number)
 int sumNumbers = SumNumbers(num);
 
 Console.WriteLine($"произведение чисел чисел от 1 до {num} = {sumNumbers}");
+
+
+// Console.WriteLine("Введите количество элементов массива : ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int[] NameArray(int size)
+// {
+//     int[] array = new int[size]; // 
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(0, 2);
+//     }
+//     return array;
+// }
+
+// void PrintArray(int[] array)
+// {   
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i]+",");
+//     }
+// }
+
+// int[] nameArray = NameArray(num);
+// PrintArray(nameArray);
--- Task30/Program.cs
+++ Task30/Program.cs
@@ -13,7 +13,6 @@
 void MassiveZeroOne()
 {
     int[] array = new int [8];
-
     for (int i = 0; i < array.Length; i++)
     {
         array[i] = new Random().Next(0, 2);
