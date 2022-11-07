c2989701690f0b906469c243fb7aa2bc022fb4c4
Evgeny <fanofsquirrels@gmail.com>
2022-11-07 12:42:05 +0300

    Выполнено

--- Task30/Program.cs
+++ Task30/Program.cs
@@ -21,3 +21,28 @@ void MassiveZeroOne()
 }
 
 MassiveZeroOne();
+
+
+// Console.WriteLine("Введите количество элементов массива : ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int[] NameArray(int size)
+// {
+//     int[] array = new int[size]; // 
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(0, 2);
+//     }
+//     return array;
+// }
+
+// void PrintArray(int[] array)
+// {   
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i]+",");
+//     }
+// }
+
+// int[] nameArray = NameArray(num);
+// PrintArray(nameArray);
