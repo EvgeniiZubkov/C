ecaf0acfd279c09561ca89d01780526966b252be
Evgeny <fanofsquirrels@gmail.com>
2022-11-08 16:17:18 +0300

    Домашка29

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -2,3 +2,16 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
+Console.WriteLine("Массив из 8 элементов от -100 до 100:");
+
+void Array()
+{
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-100, 101);
+        Console.Write($"{array[i]} ");
+    }
+}
+
+Array();
