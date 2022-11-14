dbeed56b5e7d9941be078cccc6395360f12d57c9
Evgeny <fanofsquirrels@gmail.com>
2022-11-14 11:19:54 +0300

    Выполнено

---
+++ Task34/Program.cs
@@ -0,0 +1,24 @@
+﻿// double[] CreateArrayRndDouble(int size, int min, int max)
+// {
+//     double[] array = new double[size];
+//     Random rnd = new Random();
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = rnd.NextDouble() * (max - min) + min;
+//     }
+//     return array;
+// }
+
+
+
+// double[] CreateArrayRndDouble(int size, int min, int max)
+// {
+//     double[] array = new double[size];
+//     Random rnd = new Random();
+//     for (int i = 0; i < size; i++)
+//     {
+//         double num = rnd.NextDouble() * (max - min) + min;
+//         array[i] = Math.Round(num, 1);
+//     }
+//     return array;
+// }
