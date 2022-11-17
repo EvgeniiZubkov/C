faa100a63a843fbabf8eede118d0c1ecd8478c37
Evgeny <fanofsquirrels@gmail.com>
2022-11-17 13:37:38 +0300

    Домашка41

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -5,15 +5,40 @@
 Console.WriteLine("Какое количество чисел Вы хотите ввести?");
 int size = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите числа через запятую и пробел:");
-
-int[] CreateArrayRndInt(int size, int min, int max)
+int[] CreateArray(int len)
 {
-    int[] array = new int[size];
-    Random rnd = new Random();
-    for (int i = 0; i < size; i++)
+    int[] array = new int[len];
+
+    for (int i = 0; i < array.Length; i++)
     {
-        array[i] = rnd.Next(min, max + 1);
+        Console.WriteLine($"Введите элемент массива: {i}");
+        array[i] = Convert.ToInt32(Console.ReadLine());
     }
     return array;
 }
+
+void PrintArray (int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i < array.Length - 1) Console.Write($"{array[i]},");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int PositiveSum (int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) sum+=1;
+    }
+    return sum;
+}
+
+int[] arr = CreateArray(size);
+PrintArray(arr);
+int summ = PositiveSum(arr);
+Console.WriteLine($"Количество положительных элементов в массиве = {summ}");
