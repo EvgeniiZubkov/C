9fac879ff995f2b42c9e20d0ebb882f04ae4b5b2
Evgeny <fanofsquirrels@gmail.com>
2022-11-17 11:05:50 +0300

    Выполнено

---
+++ Task41/Program.cs
@@ -0,0 +1,19 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Какое количество чисел Вы хотите ввести?");
+int size = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите числа через запятую и пробел:");
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
