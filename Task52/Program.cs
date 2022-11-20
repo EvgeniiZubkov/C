ed33106e4d2133330ec1c9ddac422e14e8738ef6
Evgeny <fanofsquirrels@gmail.com>
2022-11-20 20:30:46 +0300

    Домашка52

---
+++ Task52/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5} |");
+            else Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine("]");
+    }
+
+}
+
+void AverageNumber(int[,] matrix)
+{
+    double average = 0;
+    int text = 1;
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(0); j++)
+        {
+        average = (average + matrix[j, i]);
+        }
+        Console.WriteLine($"Ср. арифм. столбца {text} равно:{Math.Round(average / matrix.GetLength(0), 1 )} ");
+        average = 0;
+        text++;
+    }
+}
+
+int[,] averageNumber = CreateMatrixRndInt(3, 4, -10, 10);
+PrintMatrix(averageNumber);
+AverageNumber(averageNumber);
