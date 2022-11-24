842bdc616ecfc25c5163441026021e45e2161751
Evgeny <fanofsquirrels@gmail.com>
2022-11-24 11:35:36 +0300

    Выполнено

---
+++ Task67/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumNumber(int num)
+{
+    if(num == 0) return num;
+    return num % 10 + SumNumber(num / 10);
+}
+
+int sumNumber = SumNumber(number);
+Console.WriteLine(sumNumber);
